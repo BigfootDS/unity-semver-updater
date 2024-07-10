62559d47b9f3f8c0f7e396b11e09f489b2ee8d40
Alex <alex.holder@bigfootds.com>
2024-07-10 18:52:55 +1000

    fix: remove unused imports

--- src/utils/UnityProjectVersion.ts
+++ src/utils/UnityProjectVersion.ts
@@ -1,4 +1,3 @@
-import { Bumper, BumperRecommendation } from "conventional-recommended-bump/dist";
 
 /**
  * An object mapping the data used to construct a semver string for various Unity-supported platforms, alongside some helper methods.
